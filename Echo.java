d2a82e6e832fe6bc00c9eee18576d6c1a0ba57f5
Mark Kremer <mark@socialreferral.com>
2011-11-05 22:52:11 +0100

    Added basic error handling and explanatory comments

--- Echo.java
+++ Echo.java
@@ -3,29 +3,43 @@ import net.without_brains.echo.*;
 
 public class Echo {
   public static void main( String[] args ) throws Exception {
+    // message is the message to send to the EchoService
     String message = null;
 
     if (args.length > 0) {
+      // If command line arguments were given then the first argument os ised
+      // to send to the EchoService
       message = args[0];
     } 
 
     if (message.equals("") || message == null) {
+      // If no message was defined use "Hello from Java" as the default
       message = "Hello from Java";
     }
 
-    EchoService client = new EchoService();
-    EchoPortType echo = client.getEchoPort();
+    try {
+      // Instantiate SOAP client (the code for it was generated using Apache 
+      // CXF's wsdl2java utility)
+      EchoPortType client = new EchoService().getEchoPort();
 
-    EchoMessageType request;
-    EchoMessageType response;
+      // Define request and response variables
+      EchoMessageType request;
+      EchoMessageType response;
 
-    request = new EchoMessageType();
-    request.setMessage(message);
+      // Set the message in the request object
+      request = new EchoMessageType();
+      request.setMessage(message);
 
-    response = echo.echo(request);
-    System.out.println("EchoService responded to Echo: " + response.getMessage());
+      // Call Echo and output the result
+      response = client.echo(request);
+      System.out.println("EchoService responded to Echo: " + response.getMessage());
 
-    response = echo.reverseEcho(request);
-    System.out.println("EchoService responded to ReverseEcho: " + response.getMessage());
+      // Call ReverseEcho and output the result
+      response = client.reverseEcho(request);
+      System.out.println("EchoService responded to ReverseEcho: " + response.getMessage());
+    } catch (javax.xml.ws.WebServiceException exception) {
+      // In case of an error (SOAP fault or otherwise) output the error
+      System.out.println("An error occurred while attempting to interact with the EchoService: " + exception.getMessage());
+    }
   }
 }
